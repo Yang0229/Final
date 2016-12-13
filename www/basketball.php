d8aa9aa52ceb2c80255c389474d1f9d9d6373142
Yang0229 <yang0229@algonquinlive.com>
2016-12-13 18:22:04 -0500

    Create basketball.php

---
+++ www/basketball.php
@@ -0,0 +1,23 @@
+{"teams":[{"id":172 , "name":"Chicago Bulls"}
+,{"id":63 , "name":"Toronto Raptors"}
+,{"id":112 , "name":"Golden State Warriors"}
+,{"id":432 , "name":"Houston Rockets"}
+] , "scores":[{"date":"2017-06-12", "games":[
+        {"home":172, "away":63, "home_score":66, "away_score":47},
+        {"home":112, "away":432, "home_score":21, "away_score":34}
+    ]},{"date":"2017-06-19", "games":[
+        {"home":172, "away":112, "home_score":45, "away_score":2},
+        {"home":63, "away":432, "home_score":110, "away_score":109}
+    ]},{"date":"2017-06-26", "games":[
+        {"home":172, "away":432, "home_score":7, "away_score":81},
+        {"home":63, "away":112, "home_score":61, "away_score":103}
+    ]},{"date":"2017-07-03", "games":[
+        {"home":63, "away":172, "home_score":92, "away_score":21},
+        {"home":432, "away":112, "home_score":0, "away_score":98}
+    ]},{"date":"2017-07-10", "games":[
+        {"home":112, "away":172, "home_score":117, "away_score":115},
+        {"home":432, "away":63, "home_score":24, "away_score":120}
+    ]},{"date":"2017-07-17", "games":[
+        {"home":432, "away":172, "home_score":86, "away_score":93},
+        {"home":112, "away":63, "home_score":41, "away_score":79}
+    ]}]}
